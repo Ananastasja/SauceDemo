6bf9604e844c8cfb621321ae79305b7e6934dadb
Anastasiya Staravoitava <nastua.matveeva@gmail.com>
2021-11-30 13:47:48 +0100

    added Property Reader

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.PropertyReader;
 
 public class LoginTest extends BaseTest {
 
@@ -41,7 +42,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void loginWithValidCredentialsTest() {
         loginPage.openLoginPage()
-                .login(System.getenv("username"), System.getenv("password"));
+                .login(System.getenv().getOrDefault("username", PropertyReader.getProperty("username")), System.getenv().getOrDefault("password", PropertyReader.getProperty("password")));
         Assert.assertEquals(productsPage.getProductPageUrl(), PRODUCTS_URL);
     }
 }
